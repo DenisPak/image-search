0e89a4373b5538b0072a5fb4c5b4c014e678af64
Denis <rikaho@mail.ru>
2020-08-20 22:20:55 +0800

    Updates

---
+++ precache-manifest.1f26f64b6b8a21b66c663ad5b4a84eac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103702e74d0b76ee8a93065f73c4329a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27ab071a8f021161bc37",
+    "url": "./static/css/main.67e07943.chunk.css"
+  },
+  {
+    "revision": "5ef1068ae28000f4f7fd",
+    "url": "./static/js/2.96f5ab5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.96f5ab5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ab071a8f021161bc37",
+    "url": "./static/js/main.2ae6cc56.chunk.js"
+  },
+  {
+    "revision": "401ee1ba36b9ab4163de",
+    "url": "./static/js/runtime-main.a6bf46d3.js"
+  }
+]);
